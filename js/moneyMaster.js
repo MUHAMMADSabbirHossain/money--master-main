b0bec641204b2c69d68a2738d7ad8df0d74931dd
muhammad bin ahmad <dustobalok000@gmail.com>
2023-07-23 10:08:22 +0600

    getInputValueById() function added

--- js/moneyMaster.js
+++ js/moneyMaster.js
@@ -1,1 +1,14 @@
-console.log('money master file')
+console.log('money master file');
+
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+
+    const incomeInput = getInputValueById('income-input');
+    const foodExpensesInput = getInputValueById('food-expenses-input');
+    const rentExpensesInput = getInputValueById('rent-expenses-input');
+    const clothesExpensesInput = getInputValueById('clothes-expenses-input');
+
+
+
+})
+
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,9 @@
-console.log('utilites file')
+console.log('utilites file');
+
+function getInputValueById(elementInput) {
+    const elementInputField = document.getElementById(elementInput);
+    const elementInputstring = elementInputField.value;
+    const elementValue = parseFloat(elementInputstring)
+    return elementValue;
+}
+
